aaec0d65286f03f522031d385ae0a8a4bbcf62d2
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-02-08 21:10:49 +0300

    add translations

---
+++ apps/auroraApp/demo/translations/ru.auroraos.demo.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="24"/>
+        <source>Template</source>
+        <translation>Template</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="68"/>
+        <source>An unexpected error occurred, please try again later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="83"/>
+        <source>Git Viewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Shared</name>
+    <message>
+        <location filename="../qml/kmp/Shared.qml" line="56"/>
+        <source>An unexpected error occurred, please try again later</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
